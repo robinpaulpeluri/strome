79e04ae25906212d79270fdb05676091d6e35446
Robin Paul <108471951+robinpaulpeluri@users.noreply.github.com>
2024-03-05 00:06:20 +0530

    Update Storm-Breaker.py

--- Storm-Breaker.py
+++ Storm-Breaker.py
@@ -6,8 +6,6 @@ from subprocess import getoutput
 from modules import banner
 from modules import localhost
 
-
-
 if platform.uname()[0] == "Windows":
     print("\n This Tool Only Works On Linux Distributions\n")
     exit()
@@ -18,7 +16,6 @@ root = os.environ.get('USER')
 if root != "root":
     exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
 
-
 check_php = getoutput("php -v")
 if "not found" in check_php:
     exit("please install php \n command > sudo apt install php")
@@ -26,19 +23,15 @@ if "not found" in check_php:
 try:
     from colorama import Fore 
     import requests
-    from pyngrok import ngrok
     
 except ImportError:
     print("please install library \n command > python3 -m pip install -r requirments.txt")
 
-
 while True:
     banner.banner()
     banner.infolist0()
-    
 
     try:
-
         input1 = input(Fore.RED+" ┌─["+Fore.LIGHTGREEN_EX+"STORM-BREAKER"+Fore.BLUE+"~"+Fore.WHITE+"@HOME"+Fore.RED+"""]
  └──╼ """+Fore.WHITE+"$ ")
         
@@ -53,8 +46,6 @@ while True:
             banner.banner()
             localhost.screen()
 
-        
-
         elif input1 == "4":
             banner.banner()
             localhost.location()
@@ -64,13 +55,8 @@ while True:
             banner.Settings()
 
         elif input1 == "6":
-            print("\n God Lock :) ")
+            print("\n Good Luck :) ")
             sys.exit()
-
-        
-            
-
-
 
     except KeyboardInterrupt:
         print("")
